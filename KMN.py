4fd7b2dcc685f68fa68b41b09fac52e28b89d1c6
Stepan <stepanmixmaster666@gmail.com>
2024-04-14 18:00:06 +0300

    tgbot

---
+++ KMN.py
@@ -0,0 +1,106 @@
+import telebot
+import random
+
+a = """
+Это бот который будет играть с тобой в камень ножницы бумага
+Отправь или нажми на кнопку ножницы чтобы покозать ножницы
+Отправь или нажми на кнопку камень чтобы покозать камень
+Отправь или нажми на кнопку бумага чтобы покозать бумагу
+"""
+b = """
+Бумага побеждает камень («бумага обёртывает камень»)
+Камень побеждает ножницы («камень затупляет ножницы»)
+Ножницы побеждают бумагу («ножницы разрезают бумагу»)
+Если игроки показали одинаковый знак, то засчитывается ничья и игра переигрывается
+За каждую победу тебе начисляется один поинт
+А за каждое порожение твоему врагу
+Так же за ничью никто не получает поинтов
+После игры на экран выводится число побед с лева ваши а с права компютера
+"""  
+
+i_d = []
+user = []
+
+ritm = 0
+
+doesntwin = "Вы проиграли"
+draw = "Ничья"
+win = "Вы выиграли!!!"
+
+token = "6429473773:AAEPlPr4Oy88dJertcJVGKoYSqZxGry5HUc"
+bot = telebot.TeleBot(token)
+
+c = ["ножницы", "камень", "бумага"]
+d = random.choice(c)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+  global i_d, count, ritm
+  ritm = 0
+  for i in i_d:
+    if i == message.chat.id:
+        break
+    if ritm == len(i_d):
+        i_d.append(message.chat.id)
+        user.append([0, 0])
+        break
+    ritm+=1
+  
+
+@bot.message_handler(commands=['help'])
+def start(message):
+    global a
+    bot.send_message(message.from_user.id, a)
+
+@bot.message_handler(commands=['rules'])
+def start(message):
+    global b
+    bot.send_message(message.from_user.id, b)
+
+@bot.message_handler(commands=['бумага'])
+def start(message):
+    global d, c, ritm, user
+    if d == "ножницы":
+        bot.send_message(message.from_user.id, doesntwin)
+        user[ritm][1] +=1
+    elif d == "камень":
+        bot.send_message(message.from_user.id, win)
+        user[ritm][0] +=1
+    elif d == "бумага":
+        bot.send_message(message.from_user.id, draw)
+    d = random.choice(c)
+    bot.send_message(message.from_user.id, user[ritm][0], user[ritm][1])
+
+
+@bot.message_handler(commands=['ножницы'])
+def start(message):
+    global d, c, ritm, user
+    if d == "ножницы":
+        bot.send_message(message.from_user.id, draw)
+    elif d == "камень":
+        bot.send_message(message.from_user.id, doesntwin)
+        user[ritm][1] +=1
+    elif d == "бумага":
+        bot.send_message(message.from_user.id, win)
+        user[ritm][0] +=1
+    d = random.choice(c)
+    bot.send_message(message.from_user.id, user[ritm][0], user[ritm][1])
+
+@bot.message_handler(commands=['камень'])
+def start(message):
+    global d, c, ritm, user
+    if d == "ножницы":
+        bot.send_message(message.from_user.id, win)
+        user[ritm][0] +=1
+    elif d == "камень":
+        bot.send_message(message.from_user.id, draw)
+    elif d == "бумага":
+        bot.send_message(message.from_user.id, doesntwin)
+        user[ritm][1] +=1
+    d = random.choice(c)
+    bot.send_message(message.from_user.id, user[ritm][0], user[ritm][1])
+
+
+
+
+bot.polling(none_stop=True, interval=0, timeout=120)
